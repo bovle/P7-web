5933e39cf227f809bff94a8af343289db5544276
bovle <frederik.bl@live.dk>
2017-10-18 08:59:17 +0200

    nyt forsøg med heroku

--- server.js
+++ server.js
@@ -5,21 +5,21 @@ const http = require('http');
 const app = express();
 
 // API file for interacting with MongoDB
-const api = require('./api');
+const api = require('./server/api');
 
 // Parsers
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
 // Angular DIST output folder
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, '/dist')));
 
 // API location
 app.use('/api', api);
 
 // Send all other requests to the Angular app
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
 //Set Port
