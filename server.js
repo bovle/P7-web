7809b8055fb5d184b0c98e89f5830d8efe5373c1
bovle <frederik.bl@live.dk>
2017-10-25 11:38:39 +0200

    relay server istedet...

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
+const WebSocket = require('ws');
 const app = express();
-
-// API file for interacting with MongoDB
-const api = require('./server/api');
-
-// Parsers
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
 
 // Angular DIST output folder
 app.use(express.static(path.join(__dirname, '/dist')));
-
-// API location
-app.use('/api', api);
 
 // Send all other requests to the Angular app
 app.get('*', (req, res) => {
@@ -28,4 +18,16 @@ app.set('port', port);
 
 const server = http.createServer(app);
 
+const wws = new WebSocket.Server({server});
+
 server.listen(port, () => console.log(`Running on localhost:${port}`));
+
+wws.on("connection", (ws, req) => {
+    console.log(req.connection.remoteAddress + " connected to the server\n");
+    var externalIp = req.connection.remoteAddress;
+    ws.send(req.connection.remoteAddress);
+    ws.on("message", (data) => {
+        ws.send("extern: " + externalIp + " | internal: " + data);
+    });
+    
+})
